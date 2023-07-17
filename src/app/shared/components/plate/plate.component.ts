eda74703a033fb570a335da250982d84504eca85
platabrother <common.project.web@gmail.com>
2023-07-17 19:02:22 +0200

    <>precision aumentada<>

--- src/app/shared/components/plate/plate.component.ts
+++ src/app/shared/components/plate/plate.component.ts
@@ -3,6 +3,9 @@ import { Animation } from '@ionic/angular';
 import { AnimationService } from '@services/animation.service';
 import { NUMBERS, RED_NUMBERS, ROULETTE_VEL } from '@constants/constants';
 
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-plate',
   templateUrl: 'plate.component.html',
@@ -14,7 +17,8 @@ export class PlateComponent implements AfterViewInit {
   @ViewChild('mask') mask!: ElementRef;
   @ViewChild('plate') plate!: ElementRef;
 
-  private result: number = 5;
+  private stopSignal$ = new Subject<void>();
+  private result: number = 33;
   public rouletteNumb: number = 36;
 
   public maskText!: string;
@@ -38,30 +42,33 @@ export class PlateComponent implements AfterViewInit {
     this.innerAnimation?.play();
     //this.plateAnimation?.play();
 
-    // const finalLap: number = this.generateRandomLaps(1, 5);
-    const finalLap: number = 3;
+    //const finalLap: number = this.generateRandomLaps(1, 5);
+
+    const finalLap: number = 7;
+    const targetLaps: number =
+      this.result < this.rouletteNumb ? finalLap + 1 : finalLap;
     let lap: number = 0;
 
     if (!this.interval) {
-      this.interval = setInterval(() => {
-        if (this.rouletteNumb === this.result && lap >= finalLap) {
-          clearInterval(this.interval);
-          this.interval = undefined;
-
-          this.onPause();
-        } else {
-          if (this.rouletteNumb === 36) {
-            lap++;
-            this.rouletteNumb = 0;
+      this.interval = interval(ROULETTE_VEL / 36)
+        .pipe(takeUntil(this.stopSignal$))
+        .subscribe(() => {
+          if (this.rouletteNumb === this.result && lap >= targetLaps) {
+            this.onPause();
+          } else {
+            if (this.rouletteNumb === 36) {
+              lap++;
+              this.rouletteNumb = 0;
+            }
+            this.rouletteNumb = ++this.rouletteNumb;
           }
-          this.rouletteNumb = ++this.rouletteNumb;
-        }
-      }, ROULETTE_VEL / 36);
+        });
     }
   }
 
   public onPause(): void {
     //this.plateAnimation?.pause();
+    this.stopSignal$.next();
     this.innerAnimation?.pause();
 
     clearInterval(this.interval);
