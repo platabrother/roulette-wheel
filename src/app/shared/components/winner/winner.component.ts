9f72a00a3615e50d2af5784945da7e7d32c091d0
platabrother <common.project.web@gmail.com>
2023-09-04 17:32:03 +0200

    winner component first implementation

--- src/app/shared/components/winner/winner.component.ts
+++ src/app/shared/components/winner/winner.component.ts
@@ -8,8 +8,10 @@ import { Subscription } from 'rxjs';
 })
 export class WinnerComponent implements OnInit, OnDestroy {
   private subCountdown!: Subscription;
+  private subNextRound!: Subscription;
 
   public countdown!: number;
+  public nextRoundWinner!: string | undefined;
 
   constructor(private readonly roundService: RoundService) {}
 
@@ -17,9 +19,14 @@ export class WinnerComponent implements OnInit, OnDestroy {
     this.subCountdown = this.roundService.countdown$.subscribe(
       (value: number) => (this.countdown = value)
     );
+
+    this.subNextRound = this.roundService.nextRound$.subscribe(
+      (res) => (this.nextRoundWinner = res?.winner)
+    );
   }
 
   ngOnDestroy(): void {
     this.subCountdown.unsubscribe();
+    this.subNextRound.unsubscribe();
   }
 }
